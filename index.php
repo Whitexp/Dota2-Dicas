5663261125aff7a5e31ff197b002f6dcf3934fca
Gustavo <lp_gustavo_@hotmail.com>
2013-08-10 02:20:35 -0300

    first commit
    basical skeleton

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE>
+
+<html>
+<head>
+	<title>+_+ Docas 2 Dicas </title>
+	<style type="text/css" rel="stylesheet" href=/css/style.css></style>
+
+</head>
+
+
+<body>
+
+
+<h1>Selecione o heroi ! </h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ </body>
+
+</html>
